5271112544e4f7d11dcfc84163038190f048a4aa
shinzuu <not.shinzuu@gmail.com>
2025-07-09 12:55:18 +0600

    lab 2 SDP . basic laravel project setup

---
+++ SDP/Lab1/basicApp/resources/views/testpage.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>TEST</title>
+</head>
+<body>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quae distinctio numquam dolores rem! Aperiam exercitationem blanditiis nam eaque, veniam ex eveniet, sunt reiciendis cumque repellat, neque quisquam nostrum mollitia.</p> 
+</body>
+</html>
